87c4d190f74d6237436985988ef51a7b05937110
Ivan Zezyulya <ivan@zezyulya.com>
2014-06-05 13:50:17 +0700

    Fixed demo project

---
+++ Classes/AutoDescriptionPrinter.h
@@ -0,0 +1,36 @@
+//
+//  AutoDescriptionPrinter.h
+//  iHerb
+//
+//  Created by Ivan Zezyulya on 15.04.14.
+//  Copyright (c) 2014 aldigit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AutoDescriptionPrinter : NSObject
+
+- (void) increaseIndent;
+
+/// Same as calling printIndent, printText:line, and printNewLine
+- (void) printLine:(NSString *)line;
+
+- (void) printIndent;
+- (void) printText:(NSString *)text;
+- (void) printNewLine;
+
+- (void) decreaseIndent;
+
+- (NSString *) result;
+
+/// Default is 2.
+@property (nonatomic) NSInteger indentSpaces;
+
+/// Helps to avoid recursion if printed object graph has cycles.
+/// Call after calling printLine:.
+- (void) registerPrintedObject:(id)printedObject;
+- (BOOL) isObjectAlreadyPrinted:(id)object;
+
+@property (nonatomic, readonly) NSInteger currentIndentLevel;
+
+@end
