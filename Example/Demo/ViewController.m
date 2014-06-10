16a1355bece5a5e5fe8156e6d26e6f75442c37fd
Ivan Zezyulya <ivan@zezyulya.com>
2014-06-10 20:43:06 +0700

    Updated demo project

--- Example/Demo/ViewController.m
+++ Example/Demo/ViewController.m
@@ -7,40 +7,17 @@
 //
 
 #import "ViewController.h"
-
 #import "NSObject+AutoDescription.h"
 
 @interface Product: NSObject
-@property (nonatomic) NSNumber* totalPrice;
-@property (nonatomic) NSNumber* discount;
-@property (nonatomic) NSNumber* shipWeightLbs;
-@property (nonatomic) NSNumber* qty;
-@property (nonatomic) NSString* frontImgL;
-@property (nonatomic) NSString* frontImgM;
-@property (nonatomic) NSString* frontImgS;
-@property (nonatomic) NSString* frontImgXS;
-@property (nonatomic) NSString* prodName;
-@property (nonatomic) NSString* partNb;
-@property (nonatomic) NSString* pid;
-@property (nonatomic) NSString* stockStatus;
-@property (nonatomic) NSNumber* retailPrice;
-@property (nonatomic) NSNumber* listPrice;
-@property (nonatomic) NSNumber* savings;
-@property (nonatomic) NSNumber* discountPct;
-@property (nonatomic) NSNumber* avgRating;
-@property (nonatomic) NSNumber* ratingCount;
-@property (nonatomic) NSString* prodCategory;
+@property (nonatomic) NSString *title;
+@property (nonatomic) NSDecimalNumber *price;
+@property (nonatomic) NSInteger quantity;
+@property (nonatomic) NSString *imageUrlString;
+@property (nonatomic) NSArray *availableSizes;
 @end;
 
 @implementation Product
-AUTO_DESCRIPTION
-@end
-
-@interface Cart : NSObject
-@property (nonatomic) NSArray *products;
-@end
-
-@implementation Cart
 AUTO_DESCRIPTION
 @end
 
@@ -50,78 +27,44 @@ AUTO_DESCRIPTION
 {
     [super viewDidLoad];
 
-    Cart *cart = [self makeFakeCart];
+    NSArray *array = @[ @"Hello!",
+                        @"123",
+                        @(123),
+                        @(123LL),
+                        @(123.0f),
+                        @(3.14159f),
+                        @(3.14159),
+                        @{@"id1": @[@1, @2, @3],
+                          @"id2": @[@4, @5, @6]},
+                        @"Good bye!" ];
 
-    NSLog(@"Cart: %@", cart);
-}
+    NSLog(@"Array with Auto-Description: %@", [array autoDescription]);
+    NSLog(@"Array with default description: %@", array);
 
-- (Cart *) makeFakeCart
-{
-    NSMutableArray *products = [NSMutableArray new];
-    {
-        Product *product = [Product new];
-        product.frontImgL = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.frontImgXS = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.avgRating = @(3);
-        product.ratingCount = @(2292);
-        product.prodName = @"Alba Botanica, Aloe & Green Tea Oil-Free Moisturizer, 3 oz (85 g) And this is a very long description.";
-        product.retailPrice = @(13.92);
-        product.totalPrice = @(9.15);
-        product.discount = @(0.34);
-        product.stockStatus = @"In Stock";
-        product.pid = @"4433";
-        product.qty = @(5);
-        product.shipWeightLbs = @(0.34);
-        [products addObject:product];
-    } {
-        Product *product = [Product new];
-        product.frontImgL = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.frontImgXS = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.avgRating = @(5);
-        product.ratingCount = @(99542);
-        product.prodName = @"Now Foods, Shea Butter, 7 fl oz (207 ml)";
-        product.retailPrice = @(990.24);
-        product.totalPrice = @(1468.67);
-        product.stockStatus = @"Out Of Stock";
-        product.pid = @"2345";
-        product.qty = @(1);
-        product.shipWeightLbs = nil;
-        [products addObject:product];
-    } {
-        Product *product = [Product new];
-        product.frontImgL = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.frontImgXS = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.avgRating = @(0);
-        product.ratingCount = @(0);
-        product.prodName = @"Now Foods, Shea Butter, 7 fl oz (207 ml) Alba Botanica, Aloe & Green Tea Oil-Free Moisturizer, 3 oz (85 g) And this is a very long description.";
-        product.retailPrice = nil;
-        product.totalPrice = @(1.03);
-        product.discount = @(0.34);
-        product.stockStatus = @"In Stock";
-        product.pid = @"235462475673545";
-        product.qty = @(2);
-        product.shipWeightLbs = nil;
-        [products addObject:product];
-    } {
-        Product *product = [Product new];
-        product.frontImgL = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.frontImgXS = @"http://images.iherb.com/k/NOW-04753-5.jpg";
-        product.avgRating = @(1);
-        product.ratingCount = @(1);
-        product.prodName = nil;
-        product.retailPrice = @(1.03);
-        product.totalPrice = nil;
-        product.stockStatus = @"Out Of Stock";
-        product.pid = @"1";
-        product.qty = @(1);
-        product.shipWeightLbs = @(42);
-        [products addObject:product];
-    }
+    Product *product1 = [Product new];
+    product1.title = @"AbstractServerPermission";
+    product1.price = [NSDecimalNumber decimalNumberWithString:@"42"];
+    product1.quantity = 3;
+    product1.imageUrlString = @"http://placekitten.com/150/150";
+    product1.availableSizes = @[@"S", @"M", @"L"];
+
+    Product *product2 = [Product new];
+    product2.title = @"OptimizedMutexBuilder";
+    product2.price = [NSDecimalNumber decimalNumberWithString:@"762"];
+    product2.quantity = 1;
+    product2.imageUrlString = @"http://placekitten.com/120/120";
+    product2.availableSizes = @[@"XL"];
+
+    Product *product3 = [Product new];
+    product3.title = @"FastNodeVector";
+    product3.price = [NSDecimalNumber decimalNumberWithString:@"29.5"];
+    product3.quantity = 2;
+    product3.imageUrlString = @"http://placekitten.com/125/125";
+    product3.availableSizes = @[@"M", @"L"];
+
+    NSArray *prices = @[product1, product2, product3];
     
-    Cart *cart = [Cart new];
-    cart.products = products;
-    
-    return cart;
+    NSLog(@"Objects: %@", [prices autoDescription]);
 }
 
 @end
